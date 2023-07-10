4d20fbec8b19d79e155e40673584a84de2bddc2e
S <43023659+s-tangsir@users.noreply.github.com>
2023-07-10 05:47:18 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
+  
   return (
-    <hgroup>
+    <>
+      <img src="" alt=""></img>
       <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+      <p>Members of an <b>intergalactic alliance</b><br /> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+  </>
   )
 }
 export default App
